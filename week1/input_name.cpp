1839467846025c4df3f8b2a9c085aac12e1790fe
iamchaeeon <codjs2659@gmail.com>
2024-01-12 00:32:56 +0900

    first commit

---
+++ week1/input_name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+// using namespace std 권장하지 않음 
+
+int main ( void ) {
+
+    std::string name;
+    std::cin >> name ;
+    std::cout << "hi, " << "my name is " << name << ".\n" << std::endl;
+    return 0;
+
+}
---
+++ week1/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main (void ) {
+
+    std::cout << "hi" << std::endl
+    << "my name is "
+    << "Psi" << std::endl
+    << 23'222'112 << std::endl;
+}
